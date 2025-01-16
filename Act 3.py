65eaa5977fea973b59fb868cf84f109372a94412
JJSantosh <jairaj.hp@gmail.com>
2025-01-16 20:59:41 +0530

    Add files via upload

---
+++ Act 3.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age:"))
+if age>20:
+    if age>20 and age<=30:
+        print("You are allowed in the play club")
+    else:
+        print("You are not alloowed")
+else:
+    print("You are too young")
